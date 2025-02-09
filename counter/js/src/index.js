ba63536cfa80832c1f4cc81271f182ed04442479
kumarUtsav-01 <kumarutsav171@gmail.com>
2025-02-09 15:15:04 +0530

    feat(Counter): Remove unnecessary event handler added for input box.

--- counter/js/src/index.js
+++ counter/js/src/index.js
@@ -2,20 +2,11 @@ window.addEventListener("DOMContentLoaded", () => {
   const addCountButton = document.querySelector(".addCount");
   const substractCountButton = document.querySelector(".substractCount");
   const resetCountButton = document.querySelector(".resetButton");
-  const stepByInput = document.querySelector(".stepBy");
 
   addCountButton.onclick = addCount;
   substractCountButton.onclick = substractCount;
   resetCountButton.onclick = resetCount;
-  stepByInput.onchange = updateStepCount;
 });
-
-const updateStepCount = (e) => {
-  const stepByInput = document.querySelector(".stepBy");
-  const count = e.target.value;
-
-  stepByInput.setAttribute("value", count);
-};
 
 const updateCounter = (count) => {
   const counter = document.querySelector("#count");
